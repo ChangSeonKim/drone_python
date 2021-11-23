90ca5fd38e013f6ee11d7f328a78ba1fc9015a93
ybjybj457 <ybjybj457@gmail.com>
2021-11-24 01:50:46 +0900

    opencv이용한 keyboard 조종

--- move_plan.py
+++ move_plan.py
@@ -1,10 +1,28 @@
 from djitellopy import Tello
-
+import cv2
 tello = Tello()
 tello.connect()
-tello.takeoff()
-tello.move_up(50)
-tello.move_forward(60)
-tello.move_back(60)
-tello.land()
-pass
+stream  = cv2.imread('./drone.jpg')
+#move control with keyboard
+cv2.imshow("tello image", stream)
+while True:
+    key = cv2.waitKey(1)
+    if key == ord('u'):
+        tello.move_up(50)
+    elif key == ord('t'):
+        tello.takeoff()
+    elif key == ord('f'):
+        tello.move_forward(60)
+    elif key == ord('c'):
+        tello.rotate_clockwise(90)
+    elif key == ord('b'):
+        tello.move_back(60)
+    elif key == ord('l'):
+        tello.land()
+    elif key == ord('q'):
+        break
+        
+    
+    
+            
+
